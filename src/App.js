a5106627ed7006fb1d1fcc85e5b198e6a93ce368
rashed <125754285+j22003@users.noreply.github.com>
2024-09-19 16:07:09 +0300

    hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const URL = `http://api.weatherapi.com/v1/current.json?key=91425f222d10472bbd775354241809&q=jordan&aqi=yes`;
+
 
 function App() {
+   
+    const [temp, setTemp] = useState(0);
+  useEffect(() => {
+    const feachdata = async () => {
+      const result = await fetch(URL)
+      // console.log(result.json())
+      result.json().then(json =>{
+        setTemp(json.current.temp_c)
+      })
+
+    }
+    feachdata()
+  },[])
+  const [data,setdata] = useState(
+    {
+      search: '',
+      time: ''
+    }
+  )
+  const onchagehandler = (event) => {
+    setdata({
+      ...data,
+      [event.target.name]: event.target.value
+    })
+
+  }
+  const onsubmit = (e) => {
+    e.preventDefault();
+    console.log(data)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body'>
+      <h1 className='temp'>
+        the temp now in Amman: {temp} c
+      </h1>
+      
+      <form onSubmit={onsubmit}>
+        <br></br>
+        <label htmlFor='search'><h1><strong>Enter your erea</strong></h1></label>
+        <input type='text' id='search'name='search' onChange={onchagehandler} placeholder='ex. Amman'/>
+        <br></br>
+        <label htmlFor='time'><h1><strong>Enter your erea</strong></h1></label>
+        <input type='time' id='time' name='time' onChange={onchange}/>
+        <button type='submit'>
+          submit
+        </button>
+      </form>
     </div>
   );
 }
